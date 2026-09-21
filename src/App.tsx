5ba2a90d408cae1409b216433853bb723fddd681
jehovahshamah2-cpu <jehovahshamah2@gmail.com>
2026-09-21 21:30:51 +0300

    Create initial App component with welcome message

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'system-ui', padding: 40 }}>
+      <h1>Karibu VICOBA App 🎉</h1>
+      <p>Hatua ya 1 imefanikiwa. Tunaanza kujenga moduli.</p>
+    </div>
+  );
+}
