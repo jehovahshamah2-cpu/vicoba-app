74f38a3ec54bcafb553723752300805ec1773fdc
jehovahshamah2-cpu <jehovahshamah2@gmail.com>
2026-09-22 00:08:27 +0300

    Add Mwanachama interface to types

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export interface Mwanachama {
+  id: string;
+  nambaYaMwanachama: string;
+  jina: string;
+  simu: string;
+  mahali: string;
+  tareheKujiunga: string;
+  hali: 'hai' | 'amesimama';
+  kumbuka?: string;
+}
